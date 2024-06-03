1e7bbd5b530a45087c06106b55568b0765254d63
Ben <55010051+bendejong0@users.noreply.github.com>
2024-06-03 15:59:45 -0500

    Update main.rs
    Updated stuff.

--- friend_group_tsp/src/main.rs
+++ friend_group_tsp/src/main.rs
@@ -1,8 +1,19 @@
 use std::io;
 
+//TODO: 
+// put the different functions in different files. Probably group the menu files in a file, and the solver in a file.
+// get data from each persons house (this part is INSANELY important, but it'll be annoying)
+// allow editing of the names vector provided by get_names() i.e., if you forgot to you needed to pick up someone, or if you accidentally added someone.
+// 
+
+// This function gets the initial names
 fn get_names() -> Vec::<String>{
     // initially, we'll store a name of all the people who need to be picked up
     let mut names = Vec::<String>::new();
+
+    // here's a list of valid names
+    // TODO: research a better way than to do "name".to_string();
+    let valid_names = vec!["javier".to_string(), "boris".to_string(), "voya".to_string(), "ben".to_string(), "calvin".to_string(), "jonas".to_string(), "nathan".to_string(), "izzie".to_string()];
 
     // query the user for the names
     println!("Who do you need to pick up?");
@@ -13,7 +24,7 @@ fn get_names() -> Vec::<String>{
     // I could probabyl clean this up using some kind of recursion, but thats a TODO
     while name.trim() != "q"{
         // list of friends
-        println!("Boris, Ben, Calvin, Jonas, Javier, Nathan, Voya");
+        println!("Boris, Ben, Calvin, Izzie, Jonas, Javier, Nathan, Voya");
         println!("Please input a name and hit enter. When you are done, put in q");
         // you have to clear the name for some reason.
         name.clear();
@@ -22,23 +33,30 @@ fn get_names() -> Vec::<String>{
             .expect("Failed to read line");
 
         // convert to all lowercase for easier processing
-        let name_to_add = name.trim().to_lowercase();
+        let name = name.trim().to_lowercase();
 
-        if names.contains(&name_to_add){
-            println!("{name_to_add} is already in the list!\n");
+        // if its not a valid name,
+        if !valid_names.contains(&name) && &name!="q" {
+            // tell the user,
+            println!("\nError: invalid name! Please stick to the valid names.\n");
+            continue;
+        }
+
+        if names.contains(&name){
+            println!("{name} is already in the list!\n");
             continue;
         }
 
         // if it's "q" then just break.
-        if name_to_add == "q" {
+        if name == "q" {
             break;
         }
 
         // add to the vector of names
-        names.push(name_to_add.clone());
+        names.push(name.clone());
 
         // give a confirmation.
-        println!("{} added successfully!\n", name_to_add);
+        println!("{} added successfully!\n", name);
     }
     // return the vector.
     names
@@ -46,6 +64,9 @@ fn get_names() -> Vec::<String>{
 
 fn list_names(names: Vec::<String>){
     println!("To be picked up:");
+    if names.len() == 0 {
+        println!("None! Why are you using this?");
+    }
     for name in names {
         print!("{name}, ");
     }
@@ -78,6 +99,8 @@ fn main(){
     print!("\n");
     // print them so the user can see.
     list_names(names);
+    // this is to make sure its ready to run the pathfinding algorithm
     let run_pathfinder: bool = confirm_names();
+    // actually do something later. this is to make sure its working lol.
     print!("{run_pathfinder}");
 }
